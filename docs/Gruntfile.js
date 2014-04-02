df2ac6c004960811a3d0ec10579acce0b31bb586
Ahmad Nassri <ahmad@codeinchaos.com>
2014-04-02 19:27:19 -0400

    ensuring docs are cleared at every build

--- docs/Gruntfile.js
+++ docs/Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function (grunt) {
 
             rst: {
                 files: ['*.rst'],
-                tasks: ['shell'],
+                tasks: ['clean', 'shell'],
             }
         }
     });
